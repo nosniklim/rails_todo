f49aa72ce795c98f9751451c2e7563da83393ee1
nosniklim <nosniklim_engineer@outlook.jp>
2023-10-17 22:45:07 +0000

    feat: Cardモデルのspec追加

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  it "associationを確認"
-  it "validationを確認"
+  let(:described_instance) { create(:card) }
+  describe 'associations' do
+    it { is_expected.to belong_to(:list) }
+  end
+  describe 'validations' do
+
+    it { is_expected.to validate_length_of(:title).is_at_least(1).is_at_most(255) }
+    it { is_expected.to validate_length_of(:memo).is_at_most(1000) }
+  end
+  describe 'instance methods' do
+    describe '#save' do
+      # FIXME: require FactoryBot
+      # subject { described_instance.save }
+      it { is_expected.to be_truthy }
+    end
+  end
 end
