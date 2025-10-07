4bb6945567ae7c5fff130f30299626be61c4445e
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-07 22:29:07 +0900

    fix: lint errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,11 @@ import '@fortawesome/fontawesome-free/js/all';
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
