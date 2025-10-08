bee4b850f6999ba574f602984c5366e18ec7c839
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-08 21:46:21 +0900

    fix: ESLintの設定をv9以降に修正

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
+
+export default [
+  {
+    ignores: [
+      "node_modules/",
+      "vendor/",
+      "tmp/",
+      "log/",
+      "bin/",
+      "db/",
+      "public/packs/"
+    ],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        browser: true,
+        node: true
+      }
+    },
+    plugins: {
+      import: eslintPluginImport,
+      prettier: eslintPluginPrettier,
+      "unused-imports": eslintPluginUnusedImports
+    },
+    rules: {
+      "no-console": "warn",
+      "unused-imports/no-unused-imports": "error"
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:import/recommended",
+      "plugin:prettier/recommended"
+    ]
+  }
+];
