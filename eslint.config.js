eb331d88d3b892ce9eeb2d98b15843a95ac4b339
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-09 18:54:18 +0900

    fix: lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,29 @@
-import eslintPluginImport from "eslint-plugin-import";
-import eslintPluginPrettier from "eslint-plugin-prettier";
-import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
-import eslintRecommended from "@eslint/js";
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+import eslintRecommended from '@eslint/js';
 
 export default [
   eslintRecommended.configs.recommended,
   {
-    ignores: [
-      "node_modules/",
-      "vendor/",
-      "tmp/",
-      "log/",
-      "bin/",
-      "db/",
-      "public/packs/"
-    ],
+    ignores: ['node_modules/', 'vendor/', 'tmp/', 'log/', 'bin/', 'db/', 'public/packs/'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         browser: true,
-        node: true
-      }
+        node: true,
+      },
     },
     plugins: {
       import: eslintPluginImport,
       prettier: eslintPluginPrettier,
-      "unused-imports": eslintPluginUnusedImports
+      'unused-imports': eslintPluginUnusedImports,
     },
     rules: {
-      "no-console": "warn",
-      "unused-imports/no-unused-imports": "error",
-      "prettier/prettier": "error"
-    }
-  }
+      'no-console': 'warn',
+      'unused-imports/no-unused-imports': 'error',
+      'prettier/prettier': 'error',
+    },
+  },
 ];
