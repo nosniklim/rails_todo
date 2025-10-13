2dc94827d921ee341bfcdfcfd92d29b74bf157c5
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 14:49:50 +0900

    fix: uniqueness validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe User, type: :model do
   end
 
   describe 'validations' do
+    subject { create(:user) }
     it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
     it { should validate_length_of(:name).is_at_least(2).is_at_most(20) }
   end
 
