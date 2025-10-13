b36f0cdb35c2ef1366d1700eb3acd3c41e3fa149
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 14:30:29 +0900

    test: Listモデルのspec追加

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  it "associationを確認"
-  it "validationを確認"
+  let(:described_instance) { create(:list) }
+  subject { described_instance }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { should have_many(:cards).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_length_of(:title).is_at_least(1).is_at_most(255) }
+  end
+
+  describe 'include module' do
+    it { is_expected.to be_a(UtilityMethods) }
+  end
+
+  describe 'instance methods' do
+    describe '#save' do
+      subject { described_instance.save }
+      it { is_expected.to be_truthy }
+    end
+  end
 end
