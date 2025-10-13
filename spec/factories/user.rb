4424814fbcbfcd840464011f060c78a3c5a1eef6
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 13:24:08 +0900

    refactor: user factory to be unique

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { 'Test User' }
-    email { 'test@example.com' }
+    name { Faker::Name.name }
+    email { Faker::Internet.unique.email }
     password { 'password' }
     password_confirmation { 'password' }
   end
