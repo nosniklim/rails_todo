1d5dc8cbd7cb869b6854a0c071dd4c7f2de43aae
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 13:07:55 +0900

    test: fix rubocop

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :card do
-    title { "Sample Card" }
-    memo { "This is a sample card." }
+    title { 'Sample Card' }
+    memo { 'This is a sample card.' }
     position { 1 }
     association :list
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "Test User" }
-    email { "test@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
+    name { 'Test User' }
+    email { 'test@example.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
