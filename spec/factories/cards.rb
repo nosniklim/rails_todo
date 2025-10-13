532b17707072d9f10f216c3c3d42694dab95497e
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 12:35:44 +0900

    feat: add factories

---
+++ spec/factories/cards.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :card do
+    title { "Sample Card" }
+    memo { "This is a sample card." }
+    position { 1 }
+    association :list
+  end
+end
---
+++ spec/factories/lists.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :list do
+    title { "Sample List" }
+    position { 1 }
+    association :user
+  end
+end
