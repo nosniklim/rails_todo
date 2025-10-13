e1392fe39b5dc4394d70da81fca93ce3e159bcb7
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 18:09:18 +0900

    fix: rubocop

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -1,31 +1,37 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Top", type: :request do
+RSpec.describe 'Top', type: :request do
   let(:user) { create(:user) }
 
-  describe "GET /" do
-    context "ログイン時" do
+  describe 'GET /' do
+    context 'ログイン時' do
       before { sign_in user } # Devise のテストヘルパーでログイン
 
-      subject(:request!) { get root_path; response }
+      subject(:request!) do
+        get root_path
+        response
+      end
 
-      it "200 OK を返す" do
+      it '200 OK を返す' do
         expect(request!).to have_http_status(:ok)
       end
 
-      it "ヘッダーコンテンツが表示される" do
+      it 'ヘッダーコンテンツが表示される' do
         resp = request!
         expect(resp.body).to include(user.name)
-        expect(resp.body).to include("Todolist")
-        expect(resp.body).to include("New List")
-        expect(resp.body).to include("Sign out")
+        expect(resp.body).to include('Todolist')
+        expect(resp.body).to include('New List')
+        expect(resp.body).to include('Sign out')
       end
     end
 
-    context "未ログイン時" do
-      subject(:request!) { get root_path; response }
+    context '未ログイン時' do
+      subject(:request!) do
+        get root_path
+        response
+      end
 
-      it "ログインページへリダイレクトする" do
+      it 'ログインページへリダイレクトする' do
         expect(request!).to have_http_status(:found)
         expect(response).to redirect_to(new_user_session_path)
       end
