ebdb6455c646d3884227575fa0788acefeb65fe5
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 14:46:25 +0900

    feat: Userモデルのspec追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "associationを確認"
-  it "validationを確認"
+  describe 'associations' do
+    it { should have_many(:lists).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_length_of(:name).is_at_least(2).is_at_most(20) }
+  end
+
+  describe 'include module' do
+    it 'includes Devise' do
+      expect(User.ancestors).to include(Devise::Models::DatabaseAuthenticatable)
+      expect(User.ancestors).to include(Devise::Models::Registerable)
+      expect(User.ancestors).to include(Devise::Models::Recoverable)
+      expect(User.ancestors).to include(Devise::Models::Rememberable)
+      expect(User.ancestors).to include(Devise::Models::Validatable)
+    end
+  end
 end
