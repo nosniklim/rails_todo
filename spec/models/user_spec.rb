88ce794a284cb51307677b4078f99cbbf8aa118f
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 15:00:08 +0900

    refactor: user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:described_instance) { create(:user) }
+
   describe 'associations' do
     it { should have_many(:lists).dependent(:destroy) }
   end
 
   describe 'validations' do
-    subject { create(:user) }
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
     it { should validate_length_of(:name).is_at_least(2).is_at_most(20) }
@@ -19,6 +20,13 @@ RSpec.describe User, type: :model do
       expect(User.ancestors).to include(Devise::Models::Recoverable)
       expect(User.ancestors).to include(Devise::Models::Rememberable)
       expect(User.ancestors).to include(Devise::Models::Validatable)
+    end
+  end
+
+  describe 'instance methods' do
+    describe '#save' do
+      subject { described_instance.save }
+      it { is_expected.to be_truthy }
     end
   end
 end
