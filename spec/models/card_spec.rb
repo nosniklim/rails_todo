1803903553b2d10b5e8d35b030543b01142a5af4
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-13 13:02:26 +0900

    test: refactor test cases

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   let(:described_instance) { create(:card) }
+  subject { described_instance }
 
   describe 'associations' do
     it { is_expected.to belong_to(:list) }
@@ -10,6 +11,10 @@ RSpec.describe Card, type: :model do
   describe 'validations' do
     it { is_expected.to validate_length_of(:title).is_at_least(1).is_at_most(255) }
     it { is_expected.to validate_length_of(:memo).is_at_most(1000) }
+  end
+
+  describe 'include module' do
+    it { is_expected.to be_a(UtilityMethods) }
   end
 
   describe 'instance methods' do
