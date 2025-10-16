48eaed815a0576f409424ad35e24b9fde5943bf8
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-16 12:48:29 +0900

    test: ログインページ操作用page_objects作成

---
+++ spec/support/page_objects/pages/login_page.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class LoginPage
+  include Capybara::DSL
+
+  def visit!
+    visit new_user_session_path
+    self
+  end
+
+  def login(name:, password:)
+    fill_in 'Name', with: name
+    fill_in 'Password', with: password
+    click_button 'Log in'
+  end
+end
