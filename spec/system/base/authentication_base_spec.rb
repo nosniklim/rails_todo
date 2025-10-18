d11101952a9ff26a504b029c1470c822e9a7ba0a
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-18 18:33:32 +0900

    test: ログイン画面の基本機能をテストするspec追加

---
+++ spec/system/base/authentication_base_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Authentication base', type: :system do
+  let(:user) { create(:user, password: 'password') }
+
+  it 'signs in and reaches top' do
+    LoginPage.new.visit!.login(name: user.name, password: 'password')
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_selector('.header_menu')
+    expect(page).to have_link(user.name, href: edit_user_path(user))
+    expect(page).to have_link('Todolist', href: root_path)
+    expect(page).to have_link('New List', href: new_list_path)
+    expect(page).to have_link('Sign out', href: destroy_user_session_path)
+  end
+end
