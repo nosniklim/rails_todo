e96adcc812df401d48df8fa1565a9085e950a993
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-18 14:31:51 +0900

    fix: seleniumの起動設定

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,6 +4,7 @@ require 'capybara'
 
 Capybara.default_max_wait_time = 3
 Capybara.server = :puma, { Silent: true } # Pumaサーバーを使用
+Capybara.app_host = "http://app:3000"
 
 # Seleniumの設定を登録
 Capybara.register_driver :selenium_remote_chrome do |app|
@@ -11,7 +12,7 @@ Capybara.register_driver :selenium_remote_chrome do |app|
     browser: :remote,
     url: "http://#{ENV.fetch('SELENIUM_HOST', 'localhost')}:4444/wd/hub",
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      'goog:chromeOptions' => { 'args' => %w[headless disable-gpu no-sandbox disable-dev-shm-usage] }
+      'goog:chromeOptions' => { 'args' => %w[headless disable-gpu no-sandbox disable-dev-shm-usage window-size=1920,1080] }
     )
   )
 end
