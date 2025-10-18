faac58433d8850b9f62133d6d1ba7fadcbfe1460
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-18 18:51:59 +0900

    fix: rubocop errors

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,18 +4,17 @@ require 'capybara'
 
 Capybara.default_max_wait_time = 3
 Capybara.server = :puma, { Silent: true } # Pumaサーバーを使用
-Capybara.app_host = "http://app:3000"
+Capybara.app_host = 'http://app:3000'
 Capybara.save_path = 'tmp/capybara'
 
 # Seleniumの設定を登録
 Capybara.register_driver :selenium_remote_chrome do |app|
   Capybara::Selenium::Driver.new(app,
-    browser: :remote,
-    url: "http://#{ENV.fetch('SELENIUM_HOST', 'localhost')}:4444/wd/hub",
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      'goog:chromeOptions' => { 'args' => %w[headless disable-gpu no-sandbox disable-dev-shm-usage window-size=1920,1080] }
-    )
-  )
+                                 browser: :remote,
+                                 url: "http://#{ENV.fetch('SELENIUM_HOST', 'localhost')}:4444/wd/hub",
+                                 desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+                                   'goog:chromeOptions' => { 'args' => %w[headless disable-gpu no-sandbox disable-dev-shm-usage window-size=1920,1080] }
+                                 ))
 end
 
 RSpec.configure do |config|
@@ -25,7 +24,7 @@ RSpec.configure do |config|
   config.after(:each, type: :system) do |example|
     if example.exception
       FileUtils.mkdir_p('tmp/capybara')
-      timestamp = Time.now.strftime('%Y%m%d-%H%M%S')
+      timestamp = Time.current.strftime('%Y%m%d-%H%M%S')
       base_path = "#{example.full_description.parameterize}-#{timestamp}"
       page.save_screenshot("#{base_path}.png", full: true)
       save_page("#{base_path}.html")
