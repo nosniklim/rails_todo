ee743a98e3bdc86754bed20d88b3111e5158f984
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-20 15:43:23 +0900

    chore

--- spec/support/page_objects/components/header.rb
+++ spec/support/page_objects/components/header.rb
@@ -8,18 +8,22 @@ class Header
   end
 
   def goto_user_edit
+    # TODO: [data-testid="link-user-edit"]
     find('.header_menu_user .nav-link', text: @user_name).click
   end
 
   def goto_home
+    # TODO: [data-testid="link-home"]
     find('.header_menu_title .nav-link', text: 'Todolist').click
   end
 
   def goto_new_list
+    # TODO: [data-testid="link-new-list"]
     find('.header_menu_inner .nav-link', text: 'New List').click
   end
 
   def sign_out
+    # TODO: [data-testid="link-sign-out"]
     find('.header_menu_inner .nav-link', text: 'Sign out').click
   end
 end
