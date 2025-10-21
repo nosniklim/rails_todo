26fb439231f572f205b600c91e058efa13f1abb3
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-21 15:59:23 +0900

    test: カード詳細用page_objects作成

---
+++ spec/support/page_objects/pages/card_show_page.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'capybara/rspec'
+
+class CardShowPage
+  include Capybara::DSL
+
+  def have_title?(text)
+    # TODO: [data-testid="card-title"]
+    page.has_selector?('.cardContents_title', text: text)
+  end
+
+  def have_memo?(text)
+    # TODO: [data-testid="card-memo"]
+    page.has_selector?('.cardContents_memo', text: text)
+  end
+
+  def have_list?(name)
+    # TODO: [data-testid="card-list"]
+    page.has_selector?('.cardContents_list', text: name)
+  end
+
+  def have_position?(position)
+    # TODO: [data-testid="card-position"]
+    page.has_selector?('.cardContents_position', text: text)
+  end
+
+  def click_edit
+    # TODO: [data-testid="link-card-edit"]
+    find('.cardContents_btnArea .edit_btn').click
+    CardFormPage.new
+  end
+
+  def remove!
+    accept_confirm do
+      # TODO: [data-testid="link-card-remove"]
+      find('.cardContents_btnArea .delete_btn').click
+    end
+  end
+end
