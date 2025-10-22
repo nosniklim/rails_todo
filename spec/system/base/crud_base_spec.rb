a4fe796e6c00f12581bf51e3e9c3389a2eebf03e
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-22 21:29:10 +0900

    fix: seleniumじゃないとaccept_confirmが使えない

--- spec/system/base/crud_base_spec.rb
+++ spec/system/base/crud_base_spec.rb
@@ -31,17 +31,13 @@ RSpec.describe 'CRUD base', type: :system do
 
     # Remove Card
     click_link 'Task 9'
-    accept_confirm 'Are you sure you want to remove this card?' do
-      click_link 'Remove'
-    end
+    find('a[data-confirm="Are you sure you want to remove this card?"]').click
     expect(page).not_to have_content('Task 9')
 
     # Remove List
     # NOTE: リスト詳細ページへのリンクはテキストが表示されないためCSSセレクタを指定
-    within('.list_header', text: 'Todo') do
-      accept_confirm "Are you sure you want to remove 'Done'?" do
-        find('a[href*="/list/"]').click
-      end
+    within('.list_header', text: 'Done') do
+      find('a[data-confirm="Are you sure you want to remove \'Done\'?"]').click
     end
     expect(page).not_to have_content('Done')
   end
