682e5c05a6775739cf82a9386c1d64558890ace3
nosniklim <nosniklim_engineer@outlook.jp>
2025-10-24 13:14:33 +0900

    refactor: 重複したテストケースをsmokeから削除

--- spec/system/base/authentication_base_spec.rb
+++ spec/system/base/authentication_base_spec.rb
@@ -7,11 +7,7 @@ RSpec.describe 'Authentication base', type: :system do
 
   it 'ログインしてトップページに遷移すること' do
     LoginPage.new.visit!.login(name: user.name, password: 'password')
+    # ここでは遷移先のパスのみ確認（表示コンテンツは別途確認）
     expect(page).to have_current_path(root_path)
-    expect(page).to have_selector('.header_menu')
-    expect(page).to have_link(user.name, href: edit_user_path(user))
-    expect(page).to have_link('Todolist', href: root_path)
-    expect(page).to have_link('New List', href: new_list_path)
-    expect(page).to have_link('Sign out', href: destroy_user_session_path)
   end
 end
